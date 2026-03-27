108b76b610f7628234e23adf15c4f66723a8d764
Kevin González <keving5726@gmail.com>
2026-03-26 23:15:30 -0400

    Create terraform.tf file

---
+++ blue-green/terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.14.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.38.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.8.1"
+    }
+  }
+}
