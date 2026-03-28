856643ce01ef5b34988bec714c377fa98811b653
Kevin González <keving5726@gmail.com>
2026-03-27 23:01:39 -0400

    Add deployment variable

--- blue-green/modules/base/variables.tf
+++ blue-green/modules/base/variables.tf
@@ -2,3 +2,8 @@ variable "namespace" {
   description = "The project namespace to use for unique resource naming"
   type        = string
 }
+
+variable "deployment" {
+  description = "The current deployment version or environment"
+  type        = string
+}
