b54405ba391883f953c91ce5023b183bacfa9cb2
Kevin González <keving5726@gmail.com>
2026-03-31 18:35:26 -0400

    Add resource group

--- blue-green/modules/base/main.tf
+++ blue-green/modules/base/main.tf
@@ -10,3 +10,24 @@ locals {
   namespace = var.namespace != "" ? substr(join("-", [var.namespace, random_string.rand.result]), 0, 24) : random_string.rand.result
   azs       = formatlist("${data.aws_region.current.region}%s", ["a", "b"])
 }
+
+resource "aws_resourcegroups_group" "blue_green_rg" {
+  name        = "${local.namespace}-rg"
+  description = "Terraform resource group for the Blue-Green deployment"
+
+  resource_query {
+    query = <<-JSON
+{
+  "ResourceTypeFilters": [
+    "AWS::AllSupported"
+  ],
+  "TagFilters": [
+    {
+      "Key": "ResourceGroup",
+      "Values": ["${local.namespace}"]
+    }
+  ]
+}
+  JSON
+  }
+}
