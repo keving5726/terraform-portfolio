3080702497a18871d7d4e8b91942c709f6909d9f
Kevin González <keving5726@gmail.com>
2026-03-31 18:59:41 -0400

    Add Security Groups

--- blue-green/modules/base/main.tf
+++ blue-green/modules/base/main.tf
@@ -70,3 +70,28 @@ module "alb_sg" {
     Name = "Application Load Balancer SG"
   }
 }
+
+module "blue_green_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.1"
+
+  name            = "blue-green-sg"
+  use_name_prefix = false
+  description     = "Security group for the Blue-Green deployment allowing inbound HTTP traffic on port ${var.blue_green_port}"
+  vpc_id          = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["${var.private_network_cidr}"]
+  ingress_with_source_security_group_id = [
+    {
+      rule                     = "http-8080-tcp"
+      description              = "Allows inbound HTTP traffic on port ${var.blue_green_port} from the ALB security group"
+      source_security_group_id = module.alb_sg.security_group_id
+    }
+  ]
+
+  egress_with_cidr_blocks = var.default_egress_rule
+
+  tags = {
+    Name = "Blue-Green SG"
+  }
+}
