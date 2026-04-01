387f673e28b934628a0e22505de8a5139777b305
Kevin González <keving5726@gmail.com>
2026-03-31 21:19:53 -0400

    Create main.tf file

--- blue-green/main.tf
+++ blue-green/main.tf
@@ -3,3 +3,17 @@ module "base" {
   namespace  = var.namespace
   deployment = var.deployment
 }
+
+module "blue" {
+  source      = "./modules/autoscaling"
+  app_version = "v1.0"
+  label       = "blue"
+  base        = module.base
+}
+
+module "green" {
+  source      = "./modules/autoscaling"
+  app_version = "v2.0"
+  label       = "green"
+  base        = module.base
+}
