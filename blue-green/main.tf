39b1cd06e51e7a57a5e8cafd971ba38cc6932917
Kevin González <keving5726@gmail.com>
2026-04-02 11:21:24 -0400

    Add module expansion

--- blue-green/main.tf
+++ blue-green/main.tf
@@ -1,19 +1,24 @@
+locals {
+  deployment = {
+    "blue" = {
+      app_version = "v1.0"
+    },
+    "green" = {
+      app_version = "v2.0"
+    }
+  }
+}
+
 module "base" {
   source     = "./modules/base"
   namespace  = var.namespace
   deployment = var.deployment
 }
 
-module "blue" {
+module "blue_green" {
   source      = "./modules/autoscaling"
-  app_version = "v1.0"
-  label       = "blue"
-  base        = module.base
-}
-
-module "green" {
-  source      = "./modules/autoscaling"
-  app_version = "v2.0"
-  label       = "green"
+  for_each    = local.deployment
+  label       = each.key
+  app_version = each.value.app_version
   base        = module.base
 }
