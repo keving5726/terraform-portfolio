83649f0ebbb31af0d1e21c2744d77b727ac4cd8b
Kevin González <keving5726@gmail.com>
2026-04-16 19:36:47 -0400

    Add IAM role CodeBuild

--- pipeline/modules/codepipeline/iam.tf
+++ pipeline/modules/codepipeline/iam.tf
@@ -133,3 +133,14 @@ data "aws_iam_policy_document" "codepipeline" {
     resources = ["*"]
   }
 }
+
+resource "aws_iam_role" "codebuild" {
+  name               = "${local.namespace}-CodeBuild"
+  description        = "Terraform role for CodeBuild"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_codebuild.json
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
