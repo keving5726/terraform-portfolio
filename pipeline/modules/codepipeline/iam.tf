9e86f9026aee55d17b78205fe893012a3c0d37fe
Kevin González <keving5726@gmail.com>
2026-04-16 19:32:01 -0400

    Create iam.tf file

---
+++ pipeline/modules/codepipeline/iam.tf
@@ -0,0 +1,25 @@
+data "aws_iam_policy_document" "assume_role_codebuild" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "assume_role_codepipeline" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
