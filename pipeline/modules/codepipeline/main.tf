109e6f4b7d4ffaaa883a588e4ded16cfe2d94380
Kevin González <keving5726@gmail.com>
2026-04-15 23:28:42 -0400

    Add local variables

--- pipeline/modules/codepipeline/main.tf
+++ pipeline/modules/codepipeline/main.tf
@@ -6,4 +6,15 @@ resource "random_string" "rand" {
 
 locals {
   namespace = var.namespace != "" ? substr(join("-", [var.namespace, random_string.rand.result]), 0, 24) : random_string.rand.result
+
+  projects = ["plan", "apply"]
+
+  backend = templatefile("${path.module}/templates/backend.json", { config : var.s3backend_config, name : local.namespace })
+
+  pipeline_default_env = {
+    WORKING_DIRECTORY = var.working_directory
+    BACKEND           = local.backend
+  }
+
+  environment = jsonencode([for k, v in merge(var.environment, local.pipeline_default_env) : { name : k, value : v, type : "PLAINTEXT" }])
 }
