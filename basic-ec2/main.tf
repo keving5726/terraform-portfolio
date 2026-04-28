daa4b82f9f5d8a36ffee1aee6c8fdfb2301b8e4b
Kevin González <keving5726@gmail.com>
2026-04-28 12:50:45 -0400

    Update main.tf file

--- basic-ec2/main.tf
+++ basic-ec2/main.tf
@@ -1,10 +1,17 @@
+data "aws_ec2_instance_types" "free_tier" {
+  filter {
+    name   = "free-tier-eligible"
+    values = ["true"]
+  }
+}
+
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.10.20260120.4-kernel-6.12*"]
+    values = ["al2023-ami-*-arm64"]
   }
 
   filter {
@@ -39,10 +46,19 @@ data "aws_ami" "amazon_linux" {
 }
 
 resource "aws_instance" "basic_ec2" {
+  count         = var.instance_count
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
 
   tags = {
-    Name = "Basic EC2"
+    Name = "Basic EC2 ${count.index + 1}"
+  }
+
+  lifecycle {
+    precondition {
+      # Check if the chosen instance type is on the AWS Free Tier list
+      condition     = contains(data.aws_ec2_instance_types.free_tier.instance_types, var.instance_type)
+      error_message = "The '${var.instance_type}' instance type is not elegible for Free Tier in this region"
+    }
   }
 }
