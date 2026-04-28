70127654dd7f3896fa6cee0a4dfdacd5d56d1ee4
Kevin González <keving5726@gmail.com>
2026-04-28 12:58:21 -0400

    Fix order for variable parameters

--- web-server-ec2/variables.tf
+++ web-server-ec2/variables.tf
@@ -1,35 +1,35 @@
 variable "aws_region" {
-  description = "AWS Region where the instance will be deployed"
   type        = string
+  description = "AWS Region where the instance will be deployed"
   default     = "us-east-1"
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
   type        = string
+  description = "EC2 instance type"
   default     = "t4g.micro"
 }
 
 variable "server_port" {
-  description = "The port the server will use for HTTP requests"
   type        = number
+  description = "The port the server will use for HTTP requests"
   default     = 8080
 }
 
 variable "sg_name" {
-  description = "The name of the security group"
   type        = string
+  description = "The name of the security group"
   default     = "web-server-ec2-sg"
 }
 
 variable "sg_ip_protocol" {
-  description = "The IP protocol name of the security group"
   type        = string
+  description = "The IP protocol name of the security group"
   default     = "tcp"
 }
 
 variable "sg_cidr_ipv4" {
-  description = "The source IPv4 CIDR range of the security group"
   type        = string
+  description = "The source IPv4 CIDR range of the security group"
   default     = "0.0.0.0/0"
 }
