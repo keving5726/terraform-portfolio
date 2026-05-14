9ac980ae5bcb7ce0ce914af885f8f33780d32f90
Kevin González <keving5726@gmail.com>
2026-05-13 21:04:28 -0400

    Add 'environment' variable

--- multi-tier-webapp/variables.tf
+++ multi-tier-webapp/variables.tf
@@ -7,3 +7,14 @@ variable "aws_region" {
   type        = string
   description = "AWS Region where the instance will be deployed"
 }
+
+variable "environment" {
+  type        = string
+  description = "Deployment environment (dev, staging, prod)"
+  default     = "dev"
+
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "The environment variables must be one of: dev, staging, or prod"
+  }
+}
