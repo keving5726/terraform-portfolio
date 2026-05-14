43b20601e8aaadfa6c094cc90708fa9120498027
Kevin González <keving5726@gmail.com>
2026-05-13 21:16:45 -0400

    Fix formatting of the networking module variables

--- multi-tier-webapp/modules/networking/variables.tf
+++ multi-tier-webapp/modules/networking/variables.tf
@@ -1,52 +1,51 @@
 variable "namespace" {
-  description = "The project namespace to use for unique resource naming"
   type        = string
+  description = "The project namespace to use for unique resource naming"
 }
 
 variable "private_subnets" {
-  description = "List of private subnets"
   type        = list(string)
+  description = "List of private subnets"
   default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "public_subnets" {
-  description = "List of public subnets"
   type        = list(string)
+  description = "List of public subnets"
   default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
 variable "database_subnets" {
-  description = "List of database subnets"
   type        = list(string)
+  description = "List of database subnets"
   default     = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
 }
 
 variable "web_server_port" {
-  description = "The port the web server will use for HTTP requests"
   type        = number
+  description = "The port the web server will use for HTTP requests"
   default     = 8080
 }
 
 variable "alb_port" {
-  description = "The port the ALB will use for HTTP requests"
   type        = number
+  description = "The port the ALB will use for HTTP requests"
   default     = 80
 }
 
 variable "allow_all_cidr" {
-  description = "CIDR block that allows all incoming and outgoing traffic"
   type        = list(string)
+  description = "CIDR block that allows all incoming and outgoing traffic"
   default     = ["0.0.0.0/0"]
 }
 
 variable "private_network_cidr" {
-  description = "CIDR block for a private network range"
   type        = string
+  description = "CIDR block for a private network range"
   default     = "10.0.0.0/16"
 }
 
 variable "default_egress_rule" {
-  description = "Default egress rule allowing all outbound IPv4 traffic on all ports and protocols"
   type = list(object({
     from_port   = number
     to_port     = number
@@ -54,6 +53,7 @@ variable "default_egress_rule" {
     description = string
     cidr_blocks = string
   }))
+  description = "Default egress rule allowing all outbound IPv4 traffic on all ports and protocols"
   default = [
     {
       from_port   = -1
