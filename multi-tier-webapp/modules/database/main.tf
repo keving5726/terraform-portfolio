a098c1342b1ba1068cdee88af75289ece5bbb872
Kevin González <keving5726@gmail.com>
2026-05-15 10:50:56 -0400

    Replace hardcoded values with variables

--- multi-tier-webapp/modules/database/main.tf
+++ multi-tier-webapp/modules/database/main.tf
@@ -6,15 +6,15 @@ resource "random_password" "password" {
 
 resource "aws_db_instance" "database" {
   identifier             = "${var.namespace}-db"
-  allocated_storage      = 10
+  allocated_storage      = var.allocated_storage
   engine                 = "mysql"
-  engine_version         = "8.4.7"
+  engine_version         = var.db_engine_version
   instance_class         = var.db_instance_class
-  db_name                = "pets"
-  username               = "admin"
+  db_name                = var.db_name
+  username               = var.db_username
   password               = random_password.password.result
-  db_subnet_group_name   = var.vpc.database_subnet_group
-  vpc_security_group_ids = [var.sg.database]
+  db_subnet_group_name   = var.db_subnet_group_name
+  vpc_security_group_ids = [var.db_security_group]
   multi_az               = false
   skip_final_snapshot    = true
 }
