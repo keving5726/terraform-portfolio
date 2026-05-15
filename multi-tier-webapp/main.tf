b795b81a261ab9ca7d0218f7f5860edfb03948ab
Kevin González <keving5726@gmail.com>
2026-05-14 21:52:30 -0400

    Update main.tf file

--- multi-tier-webapp/main.tf
+++ multi-tier-webapp/main.tf
@@ -14,14 +14,23 @@ module "networking" {
 module "database" {
   source    = "./modules/database"
   namespace = var.namespace
-  vpc       = module.networking.vpc
-  sg        = module.networking.sg
+
+  db_name              = var.db_name
+  db_username          = var.db_username
+  db_subnet_group_name = module.networking.vpc.database_subnet_group
+  db_security_group    = module.networking.security_group_ids.database
 }
 
 module "autoscaling" {
   source    = "./modules/autoscaling"
   namespace = var.namespace
-  vpc       = module.networking.vpc
-  sg        = module.networking.sg
+
+  vpc_id              = module.networking.vpc.vpc_id
+  vpc_public_subnets  = module.networking.vpc.public_subnets
+  vpc_private_subnets = module.networking.vpc.private_subnets
+
+  alb_security_group        = module.networking.security_group_ids.alb
+  web_server_security_group = module.networking.security_group_ids.web_server
+
   db_config = module.database.db_config
 }
