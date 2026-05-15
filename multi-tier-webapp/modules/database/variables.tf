d122f226b5b269276aeab782e1c9c5a0b3f5c59b
Kevin González <keving5726@gmail.com>
2026-05-15 10:39:21 -0400

    Add validation to 'db_instance_class' variable

--- multi-tier-webapp/modules/database/variables.tf
+++ multi-tier-webapp/modules/database/variables.tf
@@ -17,4 +17,9 @@ variable "db_instance_class" {
   type        = string
   description = "DB instance class"
   default     = "db.t4g.micro"
+
+  validation {
+    condition     = can(regex("^db\\.", var.db_instance_class))
+    error_message = "The 'db_instance_class' must be a valid RDS instance type starting with 'db.'"
+  }
 }
