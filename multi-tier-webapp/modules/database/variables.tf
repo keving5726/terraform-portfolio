9796ab4993b81256067aa0a97b105f2c17d6d85f
Kevin González <keving5726@gmail.com>
2026-05-15 10:41:58 -0400

    Add required variables for RDS

--- multi-tier-webapp/modules/database/variables.tf
+++ multi-tier-webapp/modules/database/variables.tf
@@ -13,3 +13,25 @@ variable "db_instance_class" {
     error_message = "The 'db_instance_class' must be a valid RDS instance type starting with 'db.'"
   }
 }
+
+variable "db_engine_version" {
+  type        = string
+  description = "The MySQL engine version"
+  default     = "8.4.7"
+}
+
+variable "allocated_storage" {
+  type        = number
+  description = "The allocated storage in gigabytes"
+  default     = 10
+}
+
+variable "db_name" {
+  type        = string
+  description = "The name of the database to create when the DB instance is created"
+}
+
+variable "db_username" {
+  type        = string
+  description = "Username for the master DB user"
+}
