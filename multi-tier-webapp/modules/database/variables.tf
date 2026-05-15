01d9badd0881bf0d16ccfd9455eec7322b40499e
Kevin González <keving5726@gmail.com>
2026-05-15 10:40:09 -0400

    Remove networking references

--- multi-tier-webapp/modules/database/variables.tf
+++ multi-tier-webapp/modules/database/variables.tf
@@ -3,16 +3,6 @@ variable "namespace" {
   description = "The project namespace to use for unique resource naming"
 }
 
-variable "vpc" {
-  type        = any
-  description = "Reference to the VPC module outputs"
-}
-
-variable "sg" {
-  type        = any
-  description = "Security group IDs for the Application Load Balancer, web server, and database"
-}
-
 variable "db_instance_class" {
   type        = string
   description = "DB instance class"
