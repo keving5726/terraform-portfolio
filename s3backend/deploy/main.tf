44ae8174981fc988da3d240ff02e34b438a44766
Kevin González <keving5726@gmail.com>
2026-05-26 18:21:48 -0400

    Add common local variables

--- s3backend/deploy/main.tf
+++ s3backend/deploy/main.tf
@@ -1,3 +1,12 @@
+locals {
+  common_tags = {
+    Project     = var.namespace
+    Environment = var.environment
+    Owner       = var.owner
+    ManagedBy   = "Terraform"
+  }
+}
+
 module "s3backend" {
   source         = "../modules/backend"
   namespace      = var.namespace
