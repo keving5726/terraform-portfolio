81b9368a8b0b1b143442bf9729ba003e0faed93c
Kevin González <keving5726@gmail.com>
2026-05-26 17:24:09 -0400

    Fix formatting of the backend module variables

--- s3backend/modules/backend/variables.tf
+++ s3backend/modules/backend/variables.tf
@@ -1,17 +1,17 @@
 variable "namespace" {
-  description = "The project namespace to use for unique resource naming"
   type        = string
+  description = "The project namespace to use for unique resource naming"
   default     = "s3backend"
 }
 
 variable "principal_arns" {
-  description = "A list of principal ARNs allowed to assume the IAM role"
   type        = list(string)
+  description = "A list of principal ARNs allowed to assume the IAM role"
   default     = null
 }
 
 variable "force_destroy_state" {
-  description = "Force destroy the S3 bucket containing state files"
   type        = bool
+  description = "Force destroy the S3 bucket containing state files"
   default     = true
 }
