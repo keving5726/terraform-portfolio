209553a09ad005b5b6a5419c71b3804ff511c325
Kevin González <keving5726@gmail.com>
2026-06-03 21:27:06 -0400

    Fix formatting of the variables

--- s3backend-dynamodb/modules/backend/variables.tf
+++ s3backend-dynamodb/modules/backend/variables.tf
@@ -1,17 +1,17 @@
 variable "namespace" {
+  type        = string
   description = "The project namespace to use for unique resource naming"
   default     = "s3backend_dynamodb"
-  type        = string
 }
 
 variable "principal_arns" {
+  type        = list(string)
   description = "A list of principal ARNs allowed to assume the IAM role"
   default     = null
-  type        = list(string)
 }
 
 variable "force_destroy_state" {
+  type        = bool
   description = "Force destroy the S3 bucket containing state files"
   default     = true
-  type        = bool
 }
