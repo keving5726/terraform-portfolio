caecc786f32fca308b8707c5ac0942ed28cca06b
Kevin González <keving5726@gmail.com>
2026-06-04 23:46:40 -0400

    Add new variables

--- s3backend-dynamodb/deploy/variables.tf
+++ s3backend-dynamodb/deploy/variables.tf
@@ -7,5 +7,25 @@ variable "aws_region" {
 variable "namespace" {
   type        = string
   description = "The project namespace to use for unique resource naming"
-  default     = "s3backend-dynamodb"
+}
+
+variable "environment" {
+  type        = string
+  description = "Deployment environment (dev, staging, prod)"
+
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "The environment variables must be one of: dev, staging, or prod"
+  }
+}
+
+variable "owner" {
+  type        = string
+  description = "Owner or team responsible for these resources"
+}
+
+variable "principal_arns" {
+  type        = list(string)
+  description = "A list of principal ARNs allowed to assume the IAM role"
+  default     = null
 }
