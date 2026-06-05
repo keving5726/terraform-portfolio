9a704909fc07de5d44ae21f7a838c821471dfb77
Kevin González <keving5726@gmail.com>
2026-06-04 23:48:09 -0400

    Add common local variables

--- s3backend-dynamodb/deploy/main.tf
+++ s3backend-dynamodb/deploy/main.tf
@@ -1,3 +1,12 @@
+locals {
+  common_tags = {
+    Project     = var.namespace
+    Environment = var.environment
+    Owner       = var.owner
+    ManagedBy   = "Terraform"
+  }
+}
+
 module "s3backend_dynamodb" {
   source    = "../modules/backend"
   namespace = var.namespace
