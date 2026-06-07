725c49157322d6d33da1a08533821f09339244cc
Kevin González <keving5726@gmail.com>
2026-06-06 21:45:39 -0400

    Fix formatting of the variables

--- pipeline/variables.tf
+++ pipeline/variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
-  description = "AWS Region where the instance will be deployed"
   type        = string
+  description = "AWS Region where the instance will be deployed"
   default     = "us-east-1"
 }
 
 variable "namespace" {
-  description = "The project namespace to use for unique resource naming"
   type        = string
+  description = "The project namespace to use for unique resource naming"
 }
