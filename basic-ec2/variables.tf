b407f292784bb2dc0e4854f18802b3087f774c16
Kevin González <keving5726@gmail.com>
2026-06-08 19:04:21 -0400

    Update variables.tf file

--- basic-ec2/variables.tf
+++ basic-ec2/variables.tf
@@ -1,7 +1,27 @@
+variable "namespace" {
+  type        = string
+  description = "The project namespace to use for unique resource naming"
+}
+
 variable "region" {
   type        = string
   description = "AWS Region where the instance will be deployed"
   default     = "us-east-1"
+}
+
+variable "environment" {
+  type        = string
+  description = "Deployment environment (dev, staging, prod)"
+
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "The environment variables must be one of: dev, staging, or prod"
+  }
+}
+
+variable "owner" {
+  type        = string
+  description = "Owner or team responsible for these resources"
 }
 
 variable "instance_type" {
