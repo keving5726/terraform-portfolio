9cc82da3918b551b522efd37e63ccec37636415b
Kevin González <keving5726@gmail.com>
2026-06-08 23:25:11 -0400

    Remove default region

--- basic-ec2/variables.tf
+++ basic-ec2/variables.tf
@@ -6,7 +6,6 @@ variable "namespace" {
 variable "region" {
   type        = string
   description = "AWS Region where the instance will be deployed"
-  default     = "us-east-1"
 }
 
 variable "environment" {
