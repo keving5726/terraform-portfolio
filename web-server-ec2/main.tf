e2aecfd2f9626666041707c8922ce969940964e0
Kevin González <keving5726@gmail.com>
2026-06-09 23:32:24 -0400

    Update main.tf file

--- web-server-ec2/main.tf
+++ web-server-ec2/main.tf
@@ -15,8 +15,8 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-resource "aws_security_group" "web_server_ec2_sg" {
-  name        = var.sg_name
+resource "aws_security_group" "web_server_ec2" {
+  name        = var.security_group_name
   description = "Security group to allow traffic to the web server on port ${var.server_port}"
 
   tags = {
@@ -25,11 +25,11 @@ resource "aws_security_group" "web_server_ec2_sg" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ipv4" {
-  security_group_id = aws_security_group.web_server_ec2_sg.id
+  security_group_id = aws_security_group.web_server_ec2.id
 
-  cidr_ipv4   = var.sg_cidr_ipv4
+  cidr_ipv4   = var.security_group_cidr_ipv4
   from_port   = var.server_port
-  ip_protocol = var.sg_ip_protocol
+  ip_protocol = var.security_group_protocol
   to_port     = var.server_port
 }
 
@@ -37,7 +37,7 @@ resource "aws_instance" "web_server_ec2" {
   count                  = var.instance_count
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.web_server_ec2_sg.id]
+  vpc_security_group_ids = [aws_security_group.web_server_ec2.id]
 
   user_data = <<-EOF
               #!/bin/bash
