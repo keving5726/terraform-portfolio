2e574b8098f32cfea167eeb4e4596369b1e766cf
Kevin González <keving5726@gmail.com>
2026-06-09 23:29:03 -0400

    Update variables.tf file

--- web-server-ec2/variables.tf
+++ web-server-ec2/variables.tf
@@ -1,36 +1,31 @@
-variable "aws_region" {
+variable "namespace" {
+  type        = string
+  description = "The project namespace to use for unique resource naming"
+}
+
+variable "region" {
   type        = string
   description = "AWS Region where the instance will be deployed"
+}
+
+variable "environment" {
+  type        = string
+  description = "Deployment environment (dev, staging, prod)"
+
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "The environment variables must be one of: dev, staging, or prod"
+  }
+}
+
+variable "owner" {
+  type        = string
+  description = "Owner or team responsible for these resources"
 }
 
 variable "instance_type" {
   type        = string
   description = "EC2 instance type"
-  default     = "t4g.micro"
-}
-
-variable "server_port" {
-  type        = number
-  description = "The port the server will use for HTTP requests"
-  default     = 8080
-}
-
-variable "sg_name" {
-  type        = string
-  description = "The name of the security group"
-  default     = "web-server-ec2-sg"
-}
-
-variable "sg_ip_protocol" {
-  type        = string
-  description = "The IP protocol name of the security group"
-  default     = "tcp"
-}
-
-variable "sg_cidr_ipv4" {
-  type        = string
-  description = "The source IPv4 CIDR range of the security group"
-  default     = "0.0.0.0/0"
 }
 
 variable "instance_count" {
@@ -42,4 +37,25 @@ variable "instance_count" {
     condition     = var.instance_count > 0
     error_message = "This deploy requires at least one instance"
   }
+}
+
+variable "server_port" {
+  type        = number
+  description = "The port the server will use for HTTP requests"
+}
+
+variable "security_group_name" {
+  type        = string
+  description = "The name of the security group"
+}
+
+variable "security_group_protocol" {
+  type        = string
+  description = "The IP protocol name of the security group"
+  default     = "tcp"
+}
+
+variable "security_group_cidr_ipv4" {
+  type        = string
+  description = "The source IPv4 CIDR range of the security group"
 }
