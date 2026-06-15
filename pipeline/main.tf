6459f1adc1baddd22350e8cfc5e1553c3953cd2e
Kevin González <keving5726@gmail.com>
2026-06-14 22:18:42 -0400

    Add common local variables

--- pipeline/main.tf
+++ pipeline/main.tf
@@ -1,3 +1,12 @@
+locals {
+  common_tags = {
+    Project     = var.namespace
+    Environment = var.environment
+    Owner       = var.owner
+    ManagedBy   = "Terraform"
+  }
+}
+
 module "s3backend" {
   source         = "github.com/keving5726/terraform-practice-portfolio/s3backend/deploy?ref=develop"
   namespace      = var.namespace
