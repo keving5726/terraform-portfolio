9212c1c62856f199402f35c199d6273f06bb9c8e
Kevin González <keving5726@gmail.com>
2026-06-17 19:00:13 -0400

    Add 'common_tags' local variable

--- s3backend/test/main.tf
+++ s3backend/test/main.tf
@@ -1,3 +1,12 @@
+locals {
+  common_tags = {
+    Project     = var.namespace
+    Environment = var.environment
+    Owner       = var.owner
+    ManagedBy   = "Terraform"
+  }
+}
+
 resource "null_resource" "motto" {
   triggers = {
     always = timestamp()
