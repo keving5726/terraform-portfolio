72a681c899226353d521e9e4d64f8ecfa84c9c9a
Kevin González <keving5726@gmail.com>
2026-06-17 13:21:38 -0400

    Update variables.tf file

--- s3backend/deploy/variables.tf
+++ s3backend/deploy/variables.tf
@@ -1,12 +1,16 @@
-variable "aws_region" {
-  type        = string
-  description = "AWS Region where the instance will be deployed"
-  default     = "us-east-1"
-}
-
 variable "namespace" {
   type        = string
   description = "The project namespace to use for unique resource naming"
+
+  validation {
+    condition     = length(var.namespace) <= 20 && can(regex("^[a-z0-9-]+$", var.namespace))
+    error_message = "The namespace must be 20 characters or less and contain only lowercase letters, numbers, and hyphens"
+  }
+}
+
+variable "region" {
+  type        = string
+  description = "AWS Region where the instance will be deployed"
 }
 
 variable "environment" {
