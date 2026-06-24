f630261831f4954de5fa38e2d9660aa67492713d
Kevin González <keving5726@gmail.com>
2026-06-23 21:36:09 -0400

    Add random string for the namespace variable

--- pipeline/main.tf
+++ pipeline/main.tf
@@ -1,26 +1,32 @@
+resource "random_string" "rand" {
+  length  = 24
+  special = false
+  upper   = false
+}
+
 locals {
+  namespace = substr(join("-", [var.namespace, random_string.rand.result]), 0, 24)
+
   common_tags = {
-    Project     = var.namespace
+    Project     = local.namespace
     Environment = var.environment
     Owner       = var.owner
     ManagedBy   = "Terraform"
   }
 }
 
+
 module "s3backend" {
-  source         = "github.com/keving5726/terraform-practice-portfolio/s3backend/deploy?ref=develop"
-  namespace      = var.namespace
-  region         = var.region
-  environment    = var.environment
-  owner          = var.environment
+  source         = "github.com/keving5726/terraform-practice-portfolio/s3backend/modules/backend?ref=develop"
+  namespace      = local.namespace
   principal_arns = [module.codepipeline.deployment_role_arn]
 }
 
 module "codepipeline" {
   source            = "./modules/codepipeline"
-  namespace         = var.namespace
+  namespace         = local.namespace
   deployment_policy = file(var.deployment_policy)
-  s3backend_config  = module.s3backend.s3backend_config
+  s3backend_config  = module.s3backend.config
   auto_apply        = var.auto_apply
 
   pipeline_environment = var.pipeline_environment
