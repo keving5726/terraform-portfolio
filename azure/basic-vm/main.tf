b336e2e5306cb0619065ad5489912fd2322cf569
Kevin González <keving5726@gmail.com>
2026-08-26 23:55:58 -0400

    Add Azure Virtual Network

--- azure/basic-vm/main.tf
+++ azure/basic-vm/main.tf
@@ -6,3 +6,10 @@ resource "azurerm_resource_group" "main" {
   name     = "rg-${local.prefix}-001"
   location = var.location
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${local.prefix}-001"
+  address_space       = ["172.16.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
