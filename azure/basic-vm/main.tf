72acb9f52417c8ccc82e33fa1beca3b86d654790
Kevin González <keving5726@gmail.com>
2026-08-31 21:44:47 -0400

    Add Azure Network Security Group

--- azure/basic-vm/main.tf
+++ azure/basic-vm/main.tf
@@ -28,6 +28,24 @@ resource "azurerm_public_ip" "main" {
   allocation_method   = "Static"
 }
 
+resource "azurerm_network_security_group" "ssh" {
+  name                = "nsg-${local.prefix}-001"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "default-allow-ssh"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_network_interface" "main" {
   name                = "nic-${local.prefix}-001"
   location            = azurerm_resource_group.main.location
