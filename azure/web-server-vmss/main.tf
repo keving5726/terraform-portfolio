f51e88ea88593b342bfd2a2459af54f06013e390
Kevin González <keving5726@gmail.com>
2026-09-08 18:10:07 -0400

    Add Azure Load Balancer NAT rule

--- azure/web-server-vmss/main.tf
+++ azure/web-server-vmss/main.tf
@@ -73,7 +73,19 @@ resource "azurerm_lb_rule" "http" {
   frontend_port                  = 80
   backend_port                   = 80
   frontend_ip_configuration_name = "LoadBalancerFrontEnd"
-  backend_address_pool_ids = [azurerm_lb_backend_address_pool.main.id]
-  probe_id                 = azurerm_lb_probe.http.id
-  disable_outbound_snat    = true
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.main.id]
+  probe_id                       = azurerm_lb_probe.http.id
+  disable_outbound_snat          = true
+}
+
+resource "azurerm_lb_nat_rule" "ssh" {
+  name                           = "natrule-${local.prefix}-001"
+  resource_group_name            = azurerm_resource_group.main.name
+  loadbalancer_id                = azurerm_lb.external.id
+  protocol                       = "Tcp"
+  frontend_port_start            = 50000
+  frontend_port_end              = 50100
+  backend_port                   = 22
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.main.id
+  frontend_ip_configuration_name = "LoadBalancerFrontEnd"
 }
