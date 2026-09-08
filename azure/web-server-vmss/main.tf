01b9602838ac680390248ca182387fdc115139ff
Kevin González <keving5726@gmail.com>
2026-09-07 23:28:39 -0400

    Add Azure virtual network and subnet

--- azure/web-server-vmss/main.tf
+++ azure/web-server-vmss/main.tf
@@ -15,3 +15,19 @@ resource "azurerm_resource_group" "main" {
 
   tags = local.default_tags
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${local.prefix}-001"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  address_space       = ["172.16.0.0/16"]
+
+  tags = local.default_tags
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "snet-${local.prefix}-001"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["172.16.0.0/24"]
+}
