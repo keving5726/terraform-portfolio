13f7bf9c986289763a8d34f388b068cb1a47ce34
Kevin González <keving5726@gmail.com>
2026-09-08 23:50:14 -0400

    Add monitor autoscale setting

--- azure/web-server-vmss/main.tf
+++ azure/web-server-vmss/main.tf
@@ -196,3 +196,60 @@ resource "azurerm_linux_virtual_machine_scale_set" "linux" {
     ignore_changes = [instances]
   }
 }
+
+resource "azurerm_monitor_autoscale_setting" "main" {
+  name                = "autoscale-config"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  target_resource_id  = azurerm_linux_virtual_machine_scale_set.linux.id
+
+  profile {
+    name = "AutoScale"
+
+    capacity {
+      default = 2
+      minimum = 1
+      maximum = 4
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.linux.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 75
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.linux.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 25
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }
+}
